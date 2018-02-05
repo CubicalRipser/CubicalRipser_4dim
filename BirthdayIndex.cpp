a95f01bac1362535938e524c3f99b450f92851f9
Ame@candy <drowsydormouse01@gmail.com>
2018-02-05 10:54:15 +0900

    微調整

--- BirthdayIndex.cpp
+++ BirthdayIndex.cpp
@@ -1,4 +1,9 @@
-/*
+/* BirthdayIndex.cpp
+
+Copyright 2017-2018 Takeki Sudo and Kazushi Ahara.
+
+This file is part of CubicalRipser_4dim.
+
 CubicalRipser: C++ system for computation of Cubical persistence pairs
 Copyright 2017-2018 Takeki Sudo and Kazushi Ahara.
 CubicalRipser is free software: you can redistribute it and/or modify it under
@@ -11,7 +16,7 @@ persitence pairs by Ulrich Bauer, 2015-2016.  We appreciate Ulrich very much.
 We rearrange his codes of Ripser and add some new ideas for optimization on it 
 and modify it for calculation of a Cubical filtration.
 
-This file is a part of CubicalRiper, a calculator of cubical persistence pairs for 
+This part of CubicalRiper is a calculator of cubical persistence pairs for 
 4 dimensional voxel data. The input data format conforms to that of DIPHA or of PERSEUS.
  See more descriptions in README.
 
@@ -21,9 +26,6 @@ PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-
-//BirthdayIndex.cpp
 
 #include <iostream>
 
@@ -73,7 +75,6 @@ void BirthdayIndex::print(){
 bool BirthdayIndexComparator::operator()(const BirthdayIndex& o1, const BirthdayIndex& o2) const{
 	if(o1.birthday == o2.birthday){
 		if(o1.index < o2.index){
-			//return 1;
 			return true;
 		} else{
 			return false;
--- DenseCubicalGrids.cpp
+++ DenseCubicalGrids.cpp
@@ -1,5 +1,32 @@
-//
-//DenseCubicalGrids.cpp
+/* DenseCubicalGrids.cpp
+
+Copyright 2017-2018 Takeki Sudo and Kazushi Ahara.
+
+This file is part of CubicalRipser_4dim.
+
+CubicalRipser: C++ system for computation of Cubical persistence pairs
+Copyright 2017-2018 Takeki Sudo and Kazushi Ahara.
+CubicalRipser is free software: you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+CubicalRipser is deeply depending on 'Ripser', software for Vietoris-Rips 
+persitence pairs by Ulrich Bauer, 2015-2016.  We appreciate Ulrich very much.
+We rearrange his codes of Ripser and add some new ideas for optimization on it 
+and modify it for calculation of a Cubical filtration.
+
+This part of CubicalRiper is a calculator of cubical persistence pairs for 
+4 dimensional voxel data. The input data format conforms to that of DIPHA or of PERSEUS.
+ See more descriptions in README.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <algorithm>
 #include <fstream>
@@ -77,7 +104,6 @@ DenseCubicalGrids::DenseCubicalGrids(const std::string& filename, double _thresh
 		az = std::atoi(reading_line_buffer.c_str());
 		std::getline(reading_file, reading_line_buffer); 
 		aw = std::atoi(reading_line_buffer.c_str());
-		//cout << "ax : ay : az = " << ax << " : " << ay << " : " << az << endl;
 
 		for(int w = 0; w < aw + 2; ++w){
 			for(int z = 0; z < az + 2; ++z){
